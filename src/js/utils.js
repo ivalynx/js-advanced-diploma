cc2a0bfa139534164228fbe2a41c3eea7c704938
Iva Kochneva <inkybasik@gmail.com>
2020-06-22 22:58:13 +0300

    Выдача строк в utils.js реализована

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,30 @@
 export function calcTileType(index, boardSize) {
   // TODO: write logic here
-  return 'center';
+  let result = '';
+  const tileNumber = index + 1;
+  if (index >= 0 && index < 8) {
+    result = 'top';
+  } else if (index >= 56) {
+    result = 'bottom';
+  }
+  if (tileNumber % boardSize === 0) {
+    if (result === '') {
+      result = 'right';
+    } else {
+      result += '-right';
+    }
+  }
+  if (index === 0 || tileNumber % boardSize === 1) {
+    if (result === '') {
+      result = 'left';
+    } else {
+      result += '-left';
+    }
+  }
+  if (result === '') {
+    result = 'center';
+  }
+  return result;
 }
 
 export function calcHealthLevel(health) {
