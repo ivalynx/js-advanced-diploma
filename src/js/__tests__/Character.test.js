19c9bdfb01bb0fc017f2f4f013de6776bd52b3cc
Iva Kochneva <inkybasik@gmail.com>
2020-07-04 22:05:49 +0300

    Добавила тест на Запрет создания объектов Character

--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -12,10 +12,10 @@ test('Если вызываем new Character, должно выбрасыват
 test('Если вызываем new Bowerman, должно создавать персонажа', () => {
   const result = new Bowerman(1);
   expect(result).toEqual({
-    level: 1,
-    attack: 0,
-    defence: 0,
-    health: 50,
-    type: 'Bowerman',
+    "attack": 0,
+    "defence": 0,
+    "health": 50,
+    "level": 1,
+    type: "bowman",
   });
 });
