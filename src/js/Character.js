68d2565b7d450b1f89dcdfa4cfe42c2ad54ee837
Iva Kochneva <inkybasik@gmail.com>
2020-08-23 21:16:16 +0300

    Added Character.levelUp()

--- src/js/Character.js
+++ src/js/Character.js
@@ -10,4 +10,23 @@ export default class Character {
       throw (new Error('Нельзя создавать персонажа таким образом'));
     }
   }
+
+  levelUp() {
+    if (this.health === 0) {
+      throw (new Error('Персонаж умер. Помянем персонажа'));
+    }
+    this.level += 1;
+    const attackBefore = this.attack;
+    const defenseBefore = this.defense;
+    const life = this.health;
+    const attackAfter = Math.max(attackBefore, attackBefore * (1.8 - life) / 100);
+    const defenseAfter = Math.max(defenseBefore, defenseBefore * (1.8 - life) / 100);    
+    let lifeAfter = life + 80;
+    if (lifeAfter > 100) {
+      lifeAfter = 100;
+    };
+    this.attack = attackAfter;
+    this.defense = defenseAfter;
+    this.health = lifeAfter;
+  }
 }
