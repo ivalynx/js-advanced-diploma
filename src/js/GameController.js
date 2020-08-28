699a35d82d0141fea0bb5e9b057cdd279d711bc5
Iva Kochneva <inkybasik@gmail.com>
2020-08-28 22:05:38 +0300

    Пытаемся создать команду

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,16 +1,33 @@
 import themes from './themes';
+import Team from './Team';
+import { generateTeam } from "./generators";
 
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.level = 0;
   }
 
   init() {
-    // TODO: add event listeners to gamePlay events
-    this.gamePlay.drawUi(themes[0]);
+    const storage = this.stateService.storage;
+    if(storage.length === 1) {
+      this.level = 1;
+      console.log(`Давайте поиграем`);
+      this.startNewGame();
+    } else {
     // TODO: load saved stated from stateService
+      console.log(`Посмотрим, что у вас тут было`);
+    }
+    this.gamePlay.drawUi(themes[this.level - 1]);    
+    // TODO: add event listeners to gamePlay events
   }
+
+  startNewGame() {
+    const team = new Team();
+    team.createTeam(team.user, this.level);
+    team.createTeam(team.computer, this.level);
+  };
 
   onCellClick(index) {
     // TODO: react to click
